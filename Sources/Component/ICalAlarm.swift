da0e9af0bf29686d20373a3b686680ac4f11df0d
chan614 <pchan5076@gmail.com>
2022-03-04 17:23:28 +0900

    Add ICalAlarm description

--- Sources/Component/ICalAlarm.swift
+++ Sources/Component/ICalAlarm.swift
@@ -6,16 +6,53 @@
 
 import Foundation
 
+/// Provide a grouping of component properties that define an
+/// alarm.
+///
+/// See https://tools.ietf.org/html/rfc5545#section-3.6.6
 public struct ICalAlarm: VComponent {
     public let component = ICalComponent.alarm
     
-    var action: String
-    var trigger: Date
-    var summary: String?
-    var description: String?
-    var duration: ICalendarDuration?
-    var repetition: Int?
-    var attach: String?
+    /// This property defines the action to be invoked when an
+    /// alarm is triggered.
+    ///
+    /// See https://tools.ietf.org/html/rfc5545#section-3.8.6.1
+    public var action: String
+    
+    /// This property specifies when an alarm will trigger.
+    ///
+    /// See https://tools.ietf.org/html/rfc5545#section-3.8.6.3
+    public var trigger: Date
+    
+    /// This property defines a short summary or subject for the
+    /// calendar component.
+    ///
+    /// See https://tools.ietf.org/html/rfc5545#section-3.8.1.12
+    public var summary: String?
+    
+    /// This property provides a more complete description of the
+    /// calendar component than that provided by the "SUMMARY" property.
+    ///
+    /// See https://tools.ietf.org/html/rfc5545#section-3.8.1.5
+    public var description: String?
+    
+    /// This value type is used to identify properties that contain
+    /// a duration of time.
+    ///
+    /// See https://tools.ietf.org/html/rfc5545#section-3.3.6
+    public var duration: ICalDuration?
+    
+    /// This property defines the number of times the alarm should
+    /// be repeated, after the initial trigger.
+    /// ///
+    /// See https://tools.ietf.org/html/rfc5545#section-3.8.6.2
+    public var repetition: Int?
+    
+    /// This property provides the capability to associate a
+    ///document object with a calendar component.
+    ///
+    /// See https://tools.ietf.org/html/rfc5545#section-3.8.1.1
+    public var attach: String?
     
     public var properties: [VContentLine?] {
         [
