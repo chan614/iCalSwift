040da230aebdf06fdbb53ad855e0a2f6667c7270
chan614 <pchan5076@gmail.com>
2022-05-30 15:46:33 +0900

    ICalParameter public init

--- Sources/Structure/ICalParameter.swift
+++ Sources/Structure/ICalParameter.swift
@@ -10,6 +10,11 @@ public struct ICalParameter: Equatable {
     public let key: String
     public var values: [String]
     
+    public init(key: String, values: [String]) {
+        self.key = key
+        self.values = values
+    }
+    
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.key == rhs.key
     }
